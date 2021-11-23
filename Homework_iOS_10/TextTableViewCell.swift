0a9b194572748bb1865cabd5f78d84b3e51e5bf1
Alexey Makarov <alexeynkrv@gmail.com>
2021-11-24 00:38:42 +0700

    Create TextTableViewCell.swift

---
+++ Homework_iOS_10/TextTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  TextTableViewCell.swift
+//  Homework_iOS_10
+//
+//  Created by Alexey Makarov on 16.07.2021.
+//
+
+import UIKit
+
+class TextTableViewCell: UITableViewCell {
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
